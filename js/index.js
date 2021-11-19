6580ca42a2c975e079d4c5066ed50298ca522e0f
Andre Fettermann de Andrade <andre.fettermann@trtrio.gov.br>
2021-11-19 14:51:29 -0300

    Atualização da carga dos projetos

--- js/index.js
+++ js/index.js
@@ -9,12 +9,53 @@ function deleteProject(project) {
         body: JSON.stringify({})
     }).then(response => {
         loadProjects();
-        messages.textContent = "Projeto " + project.name + " excluído com sucesso às "
-            + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+        ReactDOM.render(
+            "Projeto " + project.name + " excluído com sucesso às "
+            + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+            document.querySelector('#messages-container')
+        );
+
+       // messages.textContent = "Projeto " + project.name + " excluído com sucesso às "
+       //     + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
     }).catch(function (err) {
         console.error('Failed retrieving information', err);
         messages.textContent = err;
     });
+}
+
+function createLi(project) {
+    const li = document.createElement('li');
+    
+    const a = document.createElement('a');
+    a.href = 'scenarios.html?pid=' + project._id;
+    a.className = 'project_name';
+    a.textContent = project.name + " - " + project.description;
+    li.appendChild(a);
+
+    const actions = document.createElement('p');
+    actions.className = 'actions';
+    actions.textContent = 'Ações: ';
+
+    const editLink = document.createElement('a');
+    editLink.href = 'project_form.html?pid=' + project._id;
+    editLink.textContent = 'Editar';
+    editLink.className = 'actions_options';
+    actions.appendChild(editLink);
+
+    const deleteLink = document.createElement('a');
+    deleteLink.href = "#";
+    deleteLink.onclick = function () {
+        if (confirm('Tem certeza que deseja excluir?')) {
+            deleteProject(project);
+        } else {
+        }
+    };
+    deleteLink.textContent = 'Excluir';
+    deleteLink.className = 'actions_options';
+    actions.appendChild(deleteLink);
+    li.appendChild(actions)
+
+    return li;
 }
 
 function loadProjects() {
@@ -30,47 +71,15 @@ function loadProjects() {
             var response = xhr.responseText;
             var projects = JSON.parse(response);
 
-            var messages = document.querySelector("#messages");
+            const messages = document.querySelector('#messages-container');
             messages.textContent = projects.docs.length + " carregados às "
-                + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+            + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
 
             var ul = document.querySelector("#list");
             ul.innerHTML = "";
 
             projects.docs.forEach(function (project) {
-                var li = document.createElement('li');
-                var project_name = document.createElement('a');
-                project_name.href = 'scenarios.html?pid=' + project._id;
-                project_name.className = 'project_name';
-                //link.classList.add('');
-                project_name.textContent = project.name + " - " + project.description;
-                li.appendChild(project_name);
-
-                const actions = document.createElement('p');
-                actions.className = 'actions';
-                actions.textContent = 'Ações: ';
-
-                const editLink = document.createElement('a');
-                editLink.href = 'project_form.html?pid=' + project._id;
-                editLink.textContent = 'Editar';
-                editLink.className = 'actions_options';
-
-                const deleteLink = document.createElement('a');
-                deleteLink.href = "#";
-                deleteLink.onclick = function () {
-                    if (confirm('Tem certeza que deseja excluir?')) {
-                        deleteProject(project);
-                    } else {
-                    }
-                };
-                deleteLink.textContent = 'Excluir';
-                deleteLink.className = 'actions_options';
-
-                actions.appendChild(editLink);
-                actions.appendChild(deleteLink);
-                li.appendChild(actions)
-
-                ul.appendChild(li);
+                ul.appendChild(createLi(project));
             });
         } else {
             //erroAjax.classList.remove("invisivel");
